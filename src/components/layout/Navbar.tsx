8cf53fcc8fb03bd85213ed583d520e919317d1b5
MUNEZERO NTAGANIRA Michel <Michel@Michaels-MacBook-Pro.local>
2025-02-23 01:35:11 +0200

    fixing-logo-icon

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
             <Link href="/" className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
-              YN
+              MN
             </Link>
           </div>
           
